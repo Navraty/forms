3a43c25fc315252f6b145f8b2305eb3012db750b
Navraty <tsamezojanne@gmail.com>
2023-10-30 20:43:38 +0000

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1 className='text-xl font-bold text-blue-600 bg-blue-700'>hello je suis stella</h1>
-    </>
+    <form>
+      <input type="text" />
+      <img src="img/copileCitoyen-logo-1.png" alt="logo"/>
+      <h1 className='text-xl font-bold text-blue-600 bg-blue-700'>Formulaire d'adhesion </h1>
+      <p>Devinir un observateur citoyen de la gouvernance publique</p>
+    </form>
   )
 }
 
