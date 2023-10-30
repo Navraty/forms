da59467cdc7bef2403b18201cb6152344b3ccd70
Navraty <tsamezojanne@gmail.com>
2023-10-30 19:26:10 +0000

    initialisation-forms

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-blue-600 bg-blue-700'>hello je suis stella</h1>
+    </>
+  )
+}
+
+export default App
